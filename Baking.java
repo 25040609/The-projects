f60c0ab51522d6448bd8530898f9d7f5f371cf6e
25040609 <25040609@mvula.univen.ac.za>
2025-06-07 21:27:25 +0200

    First commit

---
+++ Baking.java
@@ -0,0 +1,13 @@
+import java.lang.foreign.SymbolLookup;
+
+public class Baking{
+    public static void main(String args []){
+        System.out.println("First decide on what you want to bake ");
+        System.out.println("take all your wet ingridients");
+        System.out.println("take all your dry ingridients");
+        System.out.println("mix floyur and magarines");
+        System.out.println("mix the flour and milk ");
+        System.out.println("then you take your pan ");
+        System.out.println("then your done");
+    }
+}
