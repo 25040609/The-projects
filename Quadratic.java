50819668a94c9703b38b173ddddbc7fcd5b166d4
25040609 <25040609@mvula.univen.ac.za>
2025-06-08 00:51:53 +0200

    First commit

--- Quadratic.java
+++ Quadratic.java
@@ -12,13 +12,13 @@ public class Quadratic {
         //but  discriminant b*b-4*a*c//
         double discriminant= b*b -4*a*c;
         if (discriminant > 0){
-            double problem1 = (-b+ Math.sqrt(discriminant))/(2*a);
+            double problem1 = (-  b+ Math.sqrt(discriminant))/(2*a);
             double problem2 = (-b-Math.sqrt(discriminant))/(2*a);
             System.out.println("the answers are of this problems");
             System.out.println("the answer of the problem is " + problem1);
             System.out.println("the answer of the problem is  "+ problem2);
 
-        
+    
         }
         else if (discriminant==0){
            double problem = -b/(2*a);
@@ -26,26 +26,9 @@ public class Quadratic {
 
           
 
-        }
-
-     discriminant = b*b -4*a*c;
-        if (discriminant < 0){
-            double problem1 = (-b+ Math.sqrt(discriminant))/(2*a);
-            double problem2 = (-b-Math.sqrt(discriminant))/(2*a);
-            System.out.println("the answers are of this problems");
-            System.out.println("the answer of the problem is " + problem1);
-            System.out.println("the answer of the problem is  "+ problem2);
-
-
-
+        } 
     }
-    else if (discriminant==0){
-           double problem = -b/(2*a);
-           System.out.println("the answer is of the problem " + problem );
+}
 
     
 
-    
-}
-    }
-}
